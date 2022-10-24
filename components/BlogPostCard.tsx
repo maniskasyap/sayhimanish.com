92bbf548b50b12c61fb41a6df96574c5113634e4
Manish Kumar <manis.kasyap@gmail.com>
2022-10-24 14:10:29 +0400

    wip:: adding medium blogs

---
+++ components/BlogPostCard.tsx
@@ -0,0 +1,65 @@
+import Link from 'next/link';
+// import useSWR from 'swr';
+import cn from 'classnames';
+import ExternalLink from './ExternalLink';
+
+// import fetcher from 'lib/fetcher';
+// import { Views } from 'lib/types';
+
+export default function BlogPostCard({
+  title,
+  href,
+  claps,
+  publisher,
+  gradient,
+}) {
+  // const { data } = useSWR<Views>(`/api/views/${slug}`, fetcher);
+  // const views = data?.total;
+
+  return (
+    <a
+      className={cn(
+        'transform hover:scale-[1.01] transition-all',
+        'rounded-xl w-full bg-gradient-to-r p-1',
+        gradient
+      )}
+      href={href}
+      target="_blank"
+    >
+      <div className="flex flex-col justify-between h-full bg-white dark:bg-gray-900 rounded-lg p-4">
+        <div className="flex flex-col md:flex-row justify-between">
+          <h4 className="text-lg md:text-lg font-medium mb-4 sm:mb-6 w-full text-gray-900 dark:text-gray-100 tracking-tight">
+            {title}
+          </h4>
+        </div>
+        <div className="flex items-center w-full text-gray-800 dark:text-gray-200 capsize">
+          {/* <ExternalLink href="https://medium.com/all-is-web">
+            All is Web
+          </ExternalLink> */}
+          <span className="hover:text-gray-600 transition">
+            <span className="text-gray-500">Published in</span> {publisher}
+          </span>
+          <div className="flex ml-auto">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="0"
+              stroke="currentColor"
+              className="w-6 h-6 fill-[#bdc3c7]"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"
+              />
+            </svg>
+            <span className="ml-1 align-baseline capsize">
+              {new Number(claps).toLocaleString()}
+            </span>
+          </div>
+        </div>
+      </div>
+    </a>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import Image from 'next/image';
-import Container from '../components/Container'
+import Link from 'next/link';
+import Container from '../components/Container';
+import BlogPostCard from '../components/BlogPostCard';
 
 const Home: NextPage = () => {
   return (
@@ -33,6 +34,63 @@ const Home: NextPage = () => {
             />
           </div>
         </div>
+        <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-6 text-black dark:text-white">
+          Blog Posts
+        </h3>
+
+        <div className="flex gap-6 flex-col w-full">
+          <BlogPostCard
+            title="Use nvm, Stay Sane"
+            href="https://medium.com/all-is-web/use-nvm-stay-sane-985db5cd6493"
+            claps={13}
+            publisher="All is Web"
+            gradient="from-[#12c2e9] via-[#c471ed] to-[#c471ed]"
+          />
+          <BlogPostCard
+            title="Angular 5 — Creating New Component"
+            href="https://betterprogramming.pub/angular-5-creating-new-component-c6c5236f8024"
+            claps={455}
+            publisher="Better Programming"
+            gradient="from-[#aa4b6b] via-[#6b6b83] to-[#2C5364]"
+          />
+          <BlogPostCard
+            title="Placeholders in sass"
+            href="https://medium.com/all-is-web/placeholders-in-sass-9fc3339dc002"
+            claps={97}
+            publisher="All is Web"
+            gradient="from-[#59C173] via-[#a17fe0] to-[#2C5364]"
+          />
+          <BlogPostCard
+            title="Angular 5 — using jquery plugins"
+            href="https://medium.com/all-is-web/angular-5-using-jquery-plugins-5edf4e642969"
+            claps={472}
+            publisher="All is Web"
+            gradient="from-[#00416A] via-[#799F0C] to-[#FFE000]"
+          />
+        </div>
+        {/* <Link href="/blog"> */}
+        <a
+          className="cursor-pointer flex mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6"
+          href="https://medium.com/@manis.kasyap"
+          target={'_blank'}
+        >
+          See all posts
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="h-6 w-6 ml-1"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
+            />
+          </svg>
+        </a>
+        {/* </Link> */}
       </div>
     </Container>
   );

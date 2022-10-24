f7b3336f1339d406875f059c4d22e8c942bd6cbc
Manish Kumar <manis.kasyap@gmail.com>
2022-10-24 15:05:50 +0400

    wip:: updating title

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,28 +40,28 @@ const Home: NextPage = () => {
 
         <div className="flex gap-6 flex-col w-full">
           <BlogPostCard
-            title="Use nvm, Stay Sane"
+            title="Using nvm to manage nodejs versions across projects"
             href="https://medium.com/all-is-web/use-nvm-stay-sane-985db5cd6493"
             claps={13}
             publisher="All is Web"
             gradient="from-[#12c2e9] via-[#c471ed] to-[#c471ed]"
           />
           <BlogPostCard
-            title="Angular 5 — Creating New Component"
+            title="Step by step guide to create a new component in Angular 5"
             href="https://betterprogramming.pub/angular-5-creating-new-component-c6c5236f8024"
             claps={455}
             publisher="Better Programming"
             gradient="from-[#aa4b6b] via-[#6b6b83] to-[#2C5364]"
           />
           <BlogPostCard
-            title="Placeholders in sass"
+            title="Concept of placeholders in scss/ sass"
             href="https://medium.com/all-is-web/placeholders-in-sass-9fc3339dc002"
             claps={97}
             publisher="All is Web"
             gradient="from-[#59C173] via-[#a17fe0] to-[#2C5364]"
           />
           <BlogPostCard
-            title="Angular 5 — using jquery plugins"
+            title="Using jquery plugins in Angular 5"
             href="https://medium.com/all-is-web/angular-5-using-jquery-plugins-5edf4e642969"
             claps={472}
             publisher="All is Web"

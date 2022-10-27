697817e06b969bedb49d99f8f47affbb9f8b9469
Manish Kumar <manis.kasyap@gmail.com>
2022-10-27 14:47:53 +0400

    WIP::adding gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,36 @@
 import '../styles/global.css';
+import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}');
+          `,
+        }}
+      />
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
